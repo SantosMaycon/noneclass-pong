7935609f8920a647f99d1f04d06c194ce928e197
SantosMaycon <mayconalves.santos14@gmail.com>
2022-11-13 22:17:24 -0200

    adding racket area limit

--- Assets/Scripts/RaqueteController.cs
+++ Assets/Scripts/RaqueteController.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class RaqueteController : MonoBehaviour {
   private Vector3 myPosition;
-  public float speed = 5f;
   public float myY;
+  public float speed = 5f;
+  public float racketLimit = 3.5f;
   // Start is called before the first frame update
   void Start() {
     myPosition = transform.position;
@@ -16,11 +17,11 @@ public class RaqueteController : MonoBehaviour {
     myPosition.y = myY;
     transform.position = myPosition;
 
-    if(Input.GetKey(KeyCode.UpArrow)) {
+    if(Input.GetKey(KeyCode.UpArrow) && myY < racketLimit) {
       myY += speed * Time.deltaTime;
     }
 
-    if(Input.GetKey(KeyCode.DownArrow)) {
+    if(Input.GetKey(KeyCode.DownArrow) && myY > -racketLimit) {
       myY -= speed * Time.deltaTime;
     }
   }
