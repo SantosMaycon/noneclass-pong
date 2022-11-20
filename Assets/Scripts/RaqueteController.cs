58146f37330732dd251cb357494e3aef913451b0
SantosMaycon <mayconalves.santos14@gmail.com>
2022-11-20 19:16:21 -0200

    refactoring IA controller

--- Assets/Scripts/RaqueteController.cs
+++ Assets/Scripts/RaqueteController.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class RaqueteController : MonoBehaviour {
   private Vector3 myPosition;
   public float myY;
-  public float speed = 5f;
+  public float speed = 5.5f;
   public float racketLimit = 3.5f;
   public bool isPlayer1;
-  public bool isAutomatic = false;
+  private bool isAutomatic = true;
   public Transform transformBall;
   // Start is called before the first frame update
   void Start() {
@@ -23,23 +23,44 @@ public class RaqueteController : MonoBehaviour {
     float deltaSpeed = speed * Time.deltaTime;
 
     if (!isAutomatic) {
-      if(Input.GetKey(KeyCode.UpArrow) && isPlayer1) {
+      if(Input.GetKey(KeyCode.W) && isPlayer1) {
         myY += deltaSpeed;
       }
 
-      if(Input.GetKey(KeyCode.DownArrow) && isPlayer1) {
+      if(Input.GetKey(KeyCode.S) && isPlayer1) {
         myY -= deltaSpeed;
       }
 
-      if(Input.GetKey(KeyCode.W) && !isPlayer1) {
+      if(Input.GetKey(KeyCode.UpArrow) && !isPlayer1) {
         myY += deltaSpeed;
       }
 
-      if(Input.GetKey(KeyCode.S) && !isPlayer1) {
+      if(Input.GetKey(KeyCode.DownArrow) && !isPlayer1) {
         myY -= deltaSpeed;
+      }
+
+      if (Input.GetKey(KeyCode.Alpha1) && isPlayer1) {
+        isAutomatic = true;
+      }
+
+      if (Input.GetKey(KeyCode.Alpha2) && !isPlayer1) {
+        isAutomatic = true;
       }
     } else {
-      myY = Mathf.Lerp(myY, transformBall.position.y, 0.02f);
+      if (transformBall.position.x < -0.2f && isPlayer1) {
+        myY = Mathf.Lerp(myY, transformBall.position.y, 0.02f);
+      }
+
+      if (transformBall.position.x > 0.2f && !isPlayer1) {
+        myY = Mathf.Lerp(myY, transformBall.position.y, 0.02f);
+      }
+    
+      if ((Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) && isPlayer1) {
+        isAutomatic = false;
+      }
+      if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow)) && !isPlayer1) {
+        isAutomatic = false;
+      }
     }
 
     if (myY < -racketLimit) {
