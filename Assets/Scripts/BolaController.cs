781167e5ae397a6d762d56e0b6a0ea7ef6419307
SantosMaycon <mayconalves.santos14@gmail.com>
2022-11-20 20:24:58 -0200

    adding sound effect when the ball collider

--- Assets/Scripts/BolaController.cs
+++ Assets/Scripts/BolaController.cs
@@ -7,6 +7,8 @@ public class BolaController : MonoBehaviour {
   public Rigidbody2D myRigidbody;
   public float speed = 5f;
   public float horizontalScreenBorder = 12f;
+  public AudioClip sound;
+  public Transform transformCamera;
   private Vector2 myVelocity;
   // Start is called before the first frame update
   void Start() {
@@ -38,6 +40,11 @@ public class BolaController : MonoBehaviour {
     if (ballX < -horizontalScreenBorder || ballX > horizontalScreenBorder) {
       SceneManager.LoadScene("Jogo");
     }
+  }
+
+  private void OnCollisionEnter2D(Collision2D other) {
+    Debug.Log(other.gameObject.name);
+    AudioSource.PlayClipAtPoint(sound, transformCamera.position);
   }
 }
 
